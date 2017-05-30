edae8f9847ced6f8a823a7b83b08c790f65e14b8
Kris Hicks <khicks@pivotal.io>
2017-05-30 11:44:11 -0700

    Restore more depends_on

--- tasks/install-pcf-gcp/terraform/sql.tf
+++ tasks/install-pcf-gcp/terraform/sql.tf
@@ -43,51 +43,61 @@ resource "google_sql_database" "uaa" {
 resource "google_sql_database" "ccdb" {
   name     = "ccdb"
   instance = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.uaa"]
 }
 
 resource "google_sql_database" "notifications" {
   name     = "notifications"
   instance = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.ccdb"]
 }
 
 resource "google_sql_database" "autoscale" {
   name     = "autoscale"
   instance = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.notifications"]
 }
 
 resource "google_sql_database" "app_usage_service" {
   name     = "app_usage_service"
   instance = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.autoscale"]
 }
 
 resource "google_sql_database" "console" {
   name     = "console"
   instance = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.app_usage_service"]
 }
 
 resource "google_sql_database" "routing" {
   name     = "routing"
   instance = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.console"]
 }
 
 resource "google_sql_database" "diego" {
   name     = "diego"
   instance = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.routing"]
 }
 
 resource "google_sql_database" "account" {
   name     = "account"
   instance = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.diego"]
 }
 
 resource "google_sql_database" "nfsvolume" {
   name     = "nfsvolume"
   instance = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.account"]
 }
 
 resource "google_sql_database" "networkpolicyserver" {
   name     = "networkpolicyserver"
   instance = "${google_sql_database_instance.master.name}"
+  depends_on = ["google_sql_database.nfsvolume"]
 }
 
 resource "google_sql_user" "diego" {
